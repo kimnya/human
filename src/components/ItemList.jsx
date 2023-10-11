920cc3168c9ba8e7cba1eefa85bc32c1f2aceff8
김냐 <ymca582@cloud.com>
2023-10-11 11:15:47 +0900

    width설정

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,77 +1,77 @@
-import React, { useContext } from "react";
-import styled from "styled-components";
-import { CtxState } from "../context/HumanCtxprovider";
-import { Link } from "react-router-dom";
+import React, { useContext } from 'react';
+import styled from 'styled-components';
+import { CtxState } from '../context/HumanCtxprovider';
+import { Link } from 'react-router-dom';
 
 const List = styled.ul`
-  display: flex;
-  justify-content: center;
-  width: 100%;
-  margin-top: 10px;
+	display: flex;
+	justify-content: center;
+	width: 95%;
+	margin: 10px auto 0;
 `;
 
 const Item = styled.li``;
 
 const ImgBox = styled.div`
-  position: relative;
-  width: 380px;
-  height: 380px;
-  &:hover > div {
-    display: block;
-  }
-  &:hover > img {
-    transform: scale(1.05);
-  }
+	position: relative;
+	width: 380px;
+	height: 380px;
+	&:hover > div {
+		display: block;
+	}
+	&:hover > img {
+		transform: scale(1.05);
+	}
 `;
 const Shadow = styled.div`
-  display: none;
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  left: 0;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  z-index: 100;
-  background-color: #000;
-  opacity: 20%;
+	display: none;
+	width: 100%;
+	height: 100%;
+	position: absolute;
+	left: 0;
+	top: 0;
+	right: 0;
+	bottom: 0;
+	z-index: 100;
+	background-color: #000;
+	opacity: 20%;
 `;
 const Img = styled.img`
-  transition: all 0.3s;
+	transition: all 0.3s;
 `;
 const Items = ({ item }) => {
-  const { name, img, price, id } = item;
-  return (
-    <Item>
-      <Link to={`/products/${id - 1}`}>
-        <ImgBox>
-          <Shadow />
-          <Img src={img} alt={name} />
-        </ImgBox>
+	const { name, img, price, id } = item;
+	return (
+		<Item>
+			<Link to={`/products/${id - 1}`}>
+				<ImgBox>
+					<Shadow />
+					<Img src={img} alt={name} />
+				</ImgBox>
 
-        <p>
-          <span>{name}</span>
-        </p>
-        <span>${price}</span>
-      </Link>
-    </Item>
-  );
+				<p>
+					<span>{name}</span>
+				</p>
+				<span>${price}</span>
+			</Link>
+		</Item>
+	);
 };
 
 const ItemList = () => {
-  const state = useContext(CtxState);
+	const state = useContext(CtxState);
 
-  const { allItem } = state;
+	const { allItem } = state;
 
-  return (
-    <>
-      <List>
-        {allItem.map((item) => {
-          return <Items key={item.id} item={item} />;
-        })}
-      </List>
-    </>
-  );
+	return (
+		<>
+			<List>
+				{allItem.map((item) => {
+					return <Items key={item.id} item={item} />;
+				})}
+			</List>
+		</>
+	);
 };
 
 export default ItemList;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,24 @@
-import React from "react";
-import styled from "styled-components";
-import bgi from "./../asset/bgi.png";
-import ItemList from "../components/ItemList";
+import React from 'react';
+import styled from 'styled-components';
+import bgi from './../asset/bgi.png';
+import ItemList from '../components/ItemList';
 
 const Bgi = styled.div`
-  width: 99vw;
-  height: 800px;
-  background-image: url(${bgi});
-  background-position: center center;
-  background-size: cover;
-  background-repeat: no-repeat;
+	width: 100%;
+	height: 800px;
+	background-image: url(${bgi});
+	background-position: center center;
+	background-size: cover;
+	background-repeat: no-repeat;
 `;
 
 const Home = () => {
-  return (
-    <>
-      <Bgi></Bgi>
-      <ItemList />
-    </>
-  );
+	return (
+		<>
+			<Bgi></Bgi>
+			<ItemList />
+		</>
+	);
 };
 
 export default Home;
